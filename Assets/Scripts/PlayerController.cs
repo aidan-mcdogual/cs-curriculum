c529ce6e9966736407bf9bbefa05e189d5d81e8f
aidan-mcdogual <amcdougal5121@student.pps.net>
2024-09-12 15:28:45 -0700

    efg

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using System.Runtime;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
-{
-    public bool overworld; 
 
+{
+    public bool overworld;
+    float xspeed;
+    float xdirection;
+    float xvector;
+    float yspeed;
+    float ydirection;
+    float yvector;
     private void Start()
     {
         GetComponentInChildren<TopDown_AnimatorController>().enabled = overworld;
         GetComponentInChildren<Platformer_AnimatorController>().enabled = !overworld; //what do you think ! means?
-        
-        
+        float xspeed = 4;
+        float xvector = 0;
+        float xdirection = 0;
+        float yspeed = 4;
+        float yvector = 0;
+        float ydirection = 0;
+
         if (overworld)
         {
             GetComponent<Rigidbody2D>().gravityScale = 0f;
@@ -25,14 +36,35 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        
+        xdirection = Input.GetAxis("horizontal");
+        {
+            xvector = xspeed * xdirection;
+
+        }
+        ydirection = Input.GetAxis("vertical");
+        {
+            yvector = yspeed * ydirection;
+        }
     }
-    
     //for organization, put other built-in Unity functions here
-    
-    
-    
-    
-    
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("coin"));
+
+        Destroy(other.gameObject);
+            }
+     void OnCollisionEnter2D(Collision2D other)
+      {
+        if (other.gameObject.CompareTag("wall"));
+        {
+            print("we hit wall");
+                }
+    }
+
+
+
+
+
+
     //after all Unity functions, your own functions can go here
 }
